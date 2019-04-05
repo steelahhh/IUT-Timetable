88bd8d1993dfd90cd170b3cf26a504cadfc3c9a1
Alexander <mrkennedyua@gmail.com>
2019-04-05 21:17:29 +0500

    Feedback string as a multi-line string

--- app/src/main/java/com/alefimenko/iuttimetable/core/data/remote/FeedbackService.kt
+++ app/src/main/java/com/alefimenko/iuttimetable/core/data/remote/FeedbackService.kt
@@ -12,49 +12,59 @@ import com.alefimenko.iuttimetable.core.data.local.model.ScheduleEntity
 
 class FeedbackService(private val context: Context) {
 
-    fun sendFeedback(throwable: Throwable?, schedule: ScheduleEntity) {
+    fun sendFeedback(schedule: ScheduleEntity, throwable: Throwable? = null) {
         sendFeedback(
-            throwable,
-            schedule.formId, schedule.groupId,
-            schedule.groupName, schedule.instituteId,
-            schedule.instituteName
+            schedule.formId,
+            schedule.groupId,
+            schedule.groupName,
+            schedule.instituteId,
+            schedule.instituteName,
+            throwable
         )
     }
 
     fun sendFeedback(
-        throwable: Throwable?,
         formId: Int,
         groupId: Int,
         groupName: String,
         instituteId: Int,
-        instituteName: String
+        instituteName: String,
+        throwable: Throwable? = null
     ) {
         val subject =
             "Обратная связь Расписание ТИУ ${if (throwable != null) "| Error report" else ""}"
         val version = context.packageManager
             .getPackageInfo(context.packageName, 0).versionName ?: "2.2.0"
-        val sb = StringBuilder().apply {
-            append("Место для сообщения:\n\n\n-----------------------------")
-            append("\nPlease don't remove this information")
-            append("\nDevice OS version: ${Build.VERSION.RELEASE}")
-            append("\nDevice brand: ${Build.BRAND}")
-            append("\nDevice model: ${Build.MODEL}")
-            append("\nDevice manufacturer: ${Build.MANUFACTURER}")
-            append("\nApp version: $version")
-            append("\nForm id: $formId")
-            append("\nGroup id: $groupId; name: $groupName")
-            append("\nInstitute id: $instituteId; name: $instituteName")
-            append(if (throwable != null) "\nError message: ${throwable.message}" else "")
-        }
+        val text = """
+            Место для сообщения:
+
+
+
+            -----------------------------
+
+            Please don't remove this information
+            Device OS version: ${Build.VERSION.RELEASE}
+            Device brand: ${Build.BRAND}
+            Device model: ${Build.MODEL}
+            Device manufacturer: ${Build.MANUFACTURER}
+            App version: $version
+            Form (id=$formId)
+            Group (id=$groupId, name=$groupName)
+            Institute (id=$instituteId, name=$instituteName)
+            ${if (throwable != null) "\nError message: ${throwable.message}" else ""}
+        """.trimIndent()
+
         context.startActivity(
             Intent.createChooser(
                 Intent(Intent.ACTION_SEND).apply {
                     type = "message/rfc822"
                     putExtra(Intent.EXTRA_EMAIL, arrayOf("iuttimetablefeedback@gmail.com"))
                     putExtra(Intent.EXTRA_SUBJECT, subject)
-                    putExtra(Intent.EXTRA_TEXT, sb.toString())
+                    putExtra(Intent.EXTRA_TEXT, text)
                 }, context.getString(R.string.str_choose_email_client)
-            )
+            ).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
         )
     }
 }
